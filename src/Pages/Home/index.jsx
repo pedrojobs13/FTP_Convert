3475b84688338208917482e638257bcb5cf70b59
Pedro Lucas Gomes de Oliveira <pedro.lg.oliveira@edu.ufes.br>
2023-01-19 11:03:18 -0300

    Update index.jsx

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -35,7 +35,7 @@ export function Home() {
   }
   function deleteFtpName(deleteFtpCard) {
     const deleteFtp = ftp.filter((fptCard) => {
-      fptCard != deleteFtpCard;
+      return fptCard != deleteFtpCard;
     });
     setFtp(deleteFtp);
   }
